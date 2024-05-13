5737eb338d29910a0ad6afd728cd6c624e2d66bb
charlesQuizlet <105207836+Damascustogah007@users.noreply.github.com>
2024-05-13 21:11:32 +0000

    BrowserStack Integration

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,12 @@
-import allure from 'allure-commandline'
+import allure from "allure-commandline";
 
 export const config = {
+  //
+  // ====================
+  // BrowserStack Configuration
+  // ====================
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
   //
   // ====================
   // Runner Configuration
@@ -60,6 +66,9 @@ export const config = {
     {
       browserName: "firefox",
     },
+    {
+      browserName: "chrome",
+    },
   ],
 
   //
@@ -109,7 +118,7 @@ export const config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  // services: [],
+  services: [["browserstack"]],
   //
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
@@ -245,10 +254,10 @@ export const config = {
    * @param {boolean} result.passed    true if test has passed, otherwise false
    * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
    */
-  afterTest: async function(test, context, { error }) {
+  afterTest: async function (test, context, { error }) {
     if (error) {
       await browser.takeScreenshot();
-  }
+    }
   },
 
   /**
@@ -291,25 +300,23 @@ export const config = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {<Object>} results object containing test results
    */
-  onComplete: function() {
-    const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
+  onComplete: function () {
+    const reportError = new Error("Could not generate Allure report");
+    const generation = allure(["generate", "allure-results", "--clean"]);
+    return new Promise((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 5000);
 
-            generation.on('exit', function(exitCode) {
-                clearTimeout(generationTimeout)
+      generation.on("exit", function (exitCode) {
+        clearTimeout(generationTimeout);
 
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
+        if (exitCode !== 0) {
+          return reject(reportError);
+        }
 
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
+        console.log("Allure report successfully generated");
+        resolve();
+      });
+    });
   },
   /**
    * Gets executed when a refresh happens.
